6dbc5be4e9f4e242946b4e15de0f6ab7b5b7c0df
alaanzr <alaanzr@gmail.com>
2015-12-15 12:01:43 +0000

    Next switch

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var http = require('http'),
   express = require('express'),
-  path =require('path');
+  path =require('path'),
+  MongoClient = require('mongodb').MongoClient,
+  Server = require('mongodb').Server,
+  CollectionDriver = require('./collectionDriver').CollectionDriver,
+  bodyParser = require('body-parser');
 
 var app = express();
 app.set('port',process.env.PORT || 3000);
@@ -8,14 +12,62 @@ app.set('port',process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req,res) {
-  res.send('<html><body><h1>Hello world</h1></body></html>');
+var mongoHost = 'localHost'; //A
+var mongoPort = 27017;
+var collectionDriver;
+
+var url = 'mongodb://localhost:27017';
+
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); //B
+mongoClient.connect(url, function(err, mongoClient) { //C
+  if (!mongoClient) {
+      console.error("Error! Exiting... Must start MongoDB first");
+      process.exit(1); //D
+  }
+  var db = mongoClient.db("MyDatabase");  //E
+  collectionDriver = new CollectionDriver(db); //F
 });
 
-app.get('/:a?/:b?/:c?', function (req,res) {
-  res.send(req.params.a + ' ' + req.params.b + ' ' + req.params.c);
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/:collection', function(req, res) { //A
+   var params = req.params; //B
+   collectionDriver.findAll(req.params.collection, function(error, objs) { //C
+    	  if (error) { res.send(400, error); } //D
+	      else {
+	          if (req.accepts('html')) { //E
+    	          res.render('data',{objects: objs, collection: req.params.collection}); //F
+              } else {
+	          res.set('Content-Type','application/json'); //G
+                  res.send(200, objs); //H
+              }
+         }
+   	});
+});
+
+app.get('/:collection/:entity', function(req, res) { //I
+   var params = req.params;
+   var entity = params.entity;
+   var collection = params.collection;
+   if (entity) {
+       collectionDriver.get(collection, entity, function(error, objs) { //J
+          if (error) { res.send(400, error); }
+          else { res.send(200, objs); } //K
+       });
+   } else {
+      res.send(400, {error: 'bad url', url: req.url});
+   }
+});
+
+app.post('/:collection', function(req, res) { //A
+    var object = req.body;
+    var collection = req.params.collection;
+    collectionDriver.save(collection, object, function(err,docs) {
+          if (err) { res.send(400, err); }
+          else { res.send(201, docs); } //B
+     });
 });
 
 app.use(function (req,res) { //1
